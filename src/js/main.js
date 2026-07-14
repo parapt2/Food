e92f058e395e97b6eb56bf58daea8ee95177409f
3mgb3000 <3mgb3000@proton.me>
2026-07-14 15:02:32 +0100

    lesson 86 fetch api

--- src/js/main.js
+++ src/js/main.js
@@ -113,7 +113,7 @@ window.addEventListener('DOMContentLoaded', () => {
         // alternative variant with toggle
         // modal.classList.toggle('show');
         document.body.style.overflow = 'hidden';
-        clearInterval(modalTimerId); 
+        clearInterval(modalTimerId);
     }
 
     modalTrigger.forEach(btn => {
@@ -251,11 +251,7 @@ window.addEventListener('DOMContentLoaded', () => {
             `;
             // form.append(statusMessage);
             form.insertAdjacentElement('afterend', statusMessage);
-           
-            const request = new XMLHttpRequest();
-            request.open('POST', 'server.php');
 
-            request.setRequestHeader('Content-type', 'application/json');
             const formData = new FormData(form);
 
             const object = {};
@@ -263,20 +259,23 @@ window.addEventListener('DOMContentLoaded', () => {
                 object[key] = value;
             });
 
-            const json = JSON.stringify(object);
-
-            request.send(json);
-
-            request.addEventListener('load', () => {
-                if (request.status === 200) {
-                    console.log(request.response);
+            fetch('server.php', {
+                method: "POST",
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(object)
+            })
+                .then(data => data.text())
+                .then(data => {
+                    console.log(data);
                     showThanksModal(message.success);
-                    form.reset();
                     statusMessage.remove();
-                } else {
+                }).catch(() => {
                     showThanksModal(message.failure);
-                }
-            });
+                }).finally(() => {
+                    form.reset();
+                });
         });
     }
 
